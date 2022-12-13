9126fd4249f6dac7e7b9d231fe724e75d4b9da5d
Russell Stewart <russell@zmosoft.com>
2022-12-13 09:27:32 -0700

    Adding a comment to Repositories class.

--- sharedWeatherPlatform/src/iosMain/kotlin/com/zmosoft/weatherplatform/repositories/Repositories.kt
+++ sharedWeatherPlatform/src/iosMain/kotlin/com/zmosoft/weatherplatform/repositories/Repositories.kt
@@ -7,6 +7,10 @@ import org.kodein.di.instance
 
 @Suppress("unused")
 class Repositories: DIAware {
+    // This class allows you to access the Repositories from iOS Swift classes. To use it in Swift,
+    // just instantiate the class and retrieve the Repository you want. e.g.:
+    //
+    // let weatherRepository = Repositories().weatherRepository
     override val di by DI.lazy {
         importAll(
             SharedModules.repositoriesModule,
