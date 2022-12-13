39aca1cc88722ea68bb7d31ce0f7cb1670910818
Russell Stewart <russell@zmosoft.com>
2022-12-13 10:17:25 -0700

    Removing WeatherIO class from APIKeys.

--- sharedWeatherPlatform/src/commonMain/kotlin/com/zmosoft/weatherplatform/api/APIKeys.kt
+++ sharedWeatherPlatform/src/commonMain/kotlin/com/zmosoft/weatherplatform/api/APIKeys.kt
@@ -2,20 +2,15 @@ package com.zmosoft.weatherplatform.api
 
 import kotlinx.serialization.Serializable
 
+// The data for this class is pulled from the apiKeys.json file for both Android and iOS.
+// To see how to set that up, look at the "resources/apiKeysSample.txt" file.
 @Serializable
 data class APIKeys(
     val openWeatherMap: OpenWeatherMap = OpenWeatherMap(),
-    val weatherIO: WeatherIO = WeatherIO(),
     val googleMaps: GoogleMaps = GoogleMaps()
 ) {
     @Serializable
     data class OpenWeatherMap(
-        val apiKey: String = "",
-        val apiHost: String = ""
-    )
-
-    @Serializable
-    data class WeatherIO(
         val apiKey: String = "",
         val apiHost: String = ""
     )
